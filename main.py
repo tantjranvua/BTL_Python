9e4b6be636ef7a2ce0a3a77227f6b8a2d04817df
tantjranvua <trinhductan10032001@gmail.com>
2023-03-17 10:21:01 +0700

    reupload main file

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ app = FastAPI()
 @app.post('/uploadfile')
 def upload_file(file:UploadFile = File(...,description = 'give heart.csv file')):
     data = pd.read_csv(file.file)
-    return data
+    return (data.to_json())
