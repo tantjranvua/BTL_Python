625e9ce01fe15d35ad8a5145dce43f528320a48f
MinH <61226996+M1st-tlu@users.noreply.github.com>
2023-03-17 12:04:07 +0700

    f

--- main.py
+++ main.py
@@ -4,30 +4,45 @@ import pandas as pd
 import numpy as np
 
 app = FastAPI()
-
-data = 0
-
-@app.post('/uploadfile')
+data = pd.DataFrame()
+@app.post('/uploadFile')
 def upload_file(file:UploadFile = File(...,description = 'give heart.csv file')):
-    data = pd.read_csv(file.file)
-    return data.to_html()
+    tmp_data = pd.read_csv(file.file)
+    app.state.df = tmp_data
+    
+    return tmp_data.to_json()
 
-@app.post('/mean_max_min')
-def get_mean_max_min(columns:str):
-    print(data)
-    col_name = columns
-    arr = np.array(data[col_name])
-    arr.max()
-    arr.min()
-    arr.mean()
-    return columns
 
 @app.get('/getDataLine/{number}')
-def get_data(number):
-    return data.head(min(number,len(data)))
+def get_data(number:int):
+    data = app.state.df
+    app.state.df = pd.DataFrame(data)
+    print(data)
+    print(data.head(min(int(number),len(data))).to_json())
+    return data.head(min(int(number),len(data))).to_json()
 
 @app.get('/getColumns')
 def get_columnns():
-    return data.columns
+    data = app.state.df
+    return {"data":data.columns}
 
+@app.get('/getDataMM/{col_name}_{value}')
+def get_Max_Min(col_name,value):    
+    if (value == 'min'):
+        data[data[col_name] == data[col_name].min()]
+    else:
+        data[data[col_name] == data[col_name].max()]
+
+
+
+@app.get('/getDa/{col_name}_{value}')
+def get_data(col_name,value):
+    arr = np.array(df[col_name])
+    if (value == "min") :
+        return arr.min()
+    elif (value == "max"):
+        return arr.max()
+    elif (value == "mean"):
+        return arr.mean()
+    return "None"
 
