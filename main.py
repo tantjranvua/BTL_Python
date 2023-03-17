d790fada56f28927ba9be60de4cc24965c86a14f
tantjranvua <trinhductan10032001@gmail.com>
2023-03-17 11:20:42 +0700

    a

--- main.py
+++ main.py
@@ -3,7 +3,19 @@ from fastapi.responses import FileResponse
 import pandas as pd
 app = FastAPI()
 
+data = 0
+
 @app.post('/uploadfile')
 def upload_file(file:UploadFile = File(...,description = 'give heart.csv file')):
     data = pd.read_csv(file.file)
-    return (data.to_json())
+    return data.to_html()
+
+@app.post('/mean_max_min')
+def get_mean_max_min(columns:str):
+    print(data)
+    col_name = columns
+    arr = np.array(data[col_name])
+    arr.max()
+    arr.min()
+    arr.mean()
+    return columns
