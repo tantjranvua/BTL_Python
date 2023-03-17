67826819e5e4329870779f70f3967ce3e7573908
tantjranvua <trinhductan10032001@gmail.com>
2023-03-17 12:05:45 +0700

    mean max min

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, HTTPException, status, File, UploadFile
 from fastapi.responses import FileResponse
 import pandas as pd
+import schema
 import numpy as np
 
 app = FastAPI()
@@ -13,14 +14,14 @@ def upload_file(file:UploadFile = File(...,description = 'give heart.csv file'))
     return data.to_html()
 
 @app.post('/mean_max_min')
-def get_mean_max_min(columns:str):
+def get_mean_max_min(column : schema.Columns):
     print(data)
-    col_name = columns
-    arr = np.array(data[col_name])
-    arr.max()
-    arr.min()
-    arr.mean()
-    return columns
+    # col_name = columns
+    # arr = np.array(data[col_name])
+    # arr.max()
+    # arr.min()
+    # arr.mean()
+    return column.name
 
 @app.get('/getDataLine/{number}')
 def get_data(number):
