5be919368d38bd3c62895403ad91186ef386d05a
tantjranvua <trinhductan10032001@gmail.com>
2023-03-18 15:18:12 +0700

    sort

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ app.state.df = pd.DataFrame()
 def upload_file(file:UploadFile = File(...,description = 'give heart.csv file')):
     tmp_data = pd.read_csv(file.file)
     app.state.df = tmp_data
-    
     return tmp_data.to_json()
 
 @app.post('/addColumns/{name_row}')
@@ -21,24 +20,12 @@ def upload_columns(name_row:str,file:UploadFile = File(...,description = 'give c
     
     return app.state.df.to_json()
 
-
 @app.post('/addRow')
 def upload_row(file:UploadFile = File(...,description = 'give row.csv file')):
     tmp_data = pd.read_csv(file.file)
     app.state.df = app.state.df.append(tmp_data, ignore_index=True)
     
     return app.state.df.to_json()
-
-@app.post('/getmean_max_min')
-def get_data(column: schema.Columns_Value):
-    arr = np.array(app.state.df[column.name])
-    if (column.value == "min") :
-        return arr.min()
-    elif (column.value == "max"):
-        return arr.max()
-    elif (column.value == "mean"):
-        return arr.mean()
-    return "None"
 
 @app.get('/getDataLine/{number}')
 def get_data(number:int):
@@ -59,13 +46,35 @@ def get_Max_Min(col_name:str,value:str):
         return data[data[col_name] == data[col_name].max()].to_json()
 
 
-@app.get('/benh/{name}')
-def numberof(name:str):
-    data = app.state.df
-    res = int((data[data[name] == 1].count()[name]))
-    # print(type(int(res)))
+@app.get('/plot/{name}_{kind}')
+def plot_Data(name:str,kind : str):
+    try :
+        fig = app.state.df[name].plot(kind=kind,  
+        figsize=(20, 16), fontsize=26).get_figure()
+        fig.savefig(name+"_"+kind+'.png')
+        return FileResponse(name+"_"+kind+'.png',filename=name+"_"+kind+'.png')
+    except:
+        return "Error"
+# --------------------------------
+@app.post('/getmean_max_min', description="Truyền vào dữ liệu dạng json gồm tên cột dữ liệu và giá trị muốn trả về (mean/max/min) \n Endpoint sẽ trả về giá trị tương ứng của cột\n")
+def get_data(column: schema.Columns_Value):
+    arr = np.array(app.state.df[column.name])
+    if (column.value == "min") :
+        return arr.min()
+    elif (column.value == "max"):
+        return arr.max()
+    elif (column.value == "mean"):
+        return arr.mean()
+    return "None"
+
+@app.post('/smoking_dead')
+def get_number_dead(data:schema.Smoking_Dead):
+    df = app.state.df
+    df = df[df["smoking"]==1]
+    pro = len(df[df["DEATH_EVENT"]==1])/len(df)
+    A = np.random.binomial(1,pro,data.people)
+    res = int(A.sum())
     return {"data": res}
-    # return 0
 
 @app.post('/get_number_smoking')
 def get_number_smoking(data:schema.Number_Smoking):
@@ -78,13 +87,11 @@ def get_number_smoking(data:schema.Number_Smoking):
     # print(type((A==data.age).sum()))
     return {"data": res}
 
-@app.post('/smoking_dead')
-def get_number_dead(data:schema.Smoking_Dead):
+@app.get('/benh/{name}')
+def numberof(name:str):
     df = app.state.df
-    df = df[df["smoking"]==1]
-    pro = len(df[df["DEATH_EVENT"]==1])/len(df)
-    A = np.random.binomial(1,pro,data.people)
-    res = int(A.sum())
+    arr = np.array(df[name])
+    res = int(arr.sum())
     return {"data": res}
 
 @app.get('/change_platelets_to_{dv}')
@@ -103,13 +110,3 @@ def change_platelets(dv:str):
 def random_print():
     pos_ = np.random.randint(len(app.state.df)-1, size=1)
     return {"Data" : app.state.df.iloc[pos_[0]]}
-
-@app.get('/plot/{name}_{kind}')
-def plot_Data(name:str,kind : str):
-    try :
-        fig = app.state.df[name].plot(kind=kind,  
-        figsize=(20, 16), fontsize=26).get_figure()
-        fig.savefig(name+"_"+kind+'.png')
-        return FileResponse(name+"_"+kind+'.png',filename=name+"_"+kind+'.png')
-    except:
-        return "Error"
